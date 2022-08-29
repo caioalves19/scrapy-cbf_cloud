eb52bd214345a10f1cda42fc9923a5152ba382e8
Caio Alves <caioluizalves1995@gmail.com>
2022-08-29 10:41:05 -0300

    teste

--- cbf_cloud/spiders/cbf.py
+++ cbf_cloud/spiders/cbf.py
@@ -6,12 +6,12 @@ class CbfSpider(scrapy.Spider):
     name = 'cbf'
     allowed_domains = ['cbf.com.br']
     start_urls = [
-        f"https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2022/{i+1}" for i in range(3)]
+        f"https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2022/{i+1}" for i in range(1)]
 
     def parse(self, response):
         numero_jogo = int(response.url.split('/')[-1].split('?')[0])
         data_jogo = response.css('.col-xs-6 span::text').get()
-        jogo = {'numero_jogo': numero_jogo, "time_mandante": "Flamengo",
+        jogo = {'numero_jogo': numero_jogo, "time_mandante": "Vasco",
                 "time_visitante": "Vasco",
                 "estadio": "São Januário",
                 "cidade": "Rio de Janeiro",
