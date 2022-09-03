e24fcd510a8f34c3655648a4b264977b48b991e8
Caio Alves <caioluizalves1995@gmail.com>
2022-09-03 11:24:31 -0300

    Versão completa para coletar jogos do Brasileirão

--- cbf_cloud/items.py
+++ cbf_cloud/items.py
@@ -4,9 +4,32 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import TakeFirst, TakeSecond, Compose
 
 
-class CbfCloudItem(scrapy.Item):
+def tratar_nome_time(time):
+    # A função apenas deixa o nome do time no padrão desejado
+
+    nome_time, estado_time = time.split(' - ')
+
+    nomes_times = {"AMÉRICA FC SAF": "América", "CUIABÁ SAF": 'Cuiabá',
+                   "ATLÉTICO MINEIRO": "Atlético", "ATHLETICO PARANAENSE": "Athletico"}
+
+    if nome_time.upper() in nomes_times:
+        return nomes_times[nome_time.upper()] + ' - ' + estado_time
+
+    return nome_time + ' - ' + estado_time
+
+
+class JogoItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    numero = scrapy.Field(output_processor=TakeFirst())
+    time_mandante = scrapy.Field(
+        output_processor=Compose(TakeFirst(), tratar_nome_time))
+    time_visitante = scrapy.Field(
+        output_processor=Compose(TakeSecond(), tratar_nome_time))
+    data = scrapy.Field(output_processor=TakeFirst())
+    hora = scrapy.Field(output_processor=TakeFirst())
+    estadio = scrapy.Field(output_processor=TakeFirst())
+    cidade = scrapy.Field(output_processor=TakeFirst())
+    estado = scrapy.Field(output_processor=TakeFirst())
--- cbf_cloud/spiders/cbf.py
+++ cbf_cloud/spiders/cbf.py
@@ -1,22 +1,60 @@
 import scrapy
 import requests
+from scrapy.loader import ItemLoader
+from cbf_cloud.items import JogoItem
 
 
 class CbfSpider(scrapy.Spider):
-    name = 'cbf'
-    allowed_domains = ['cbf.com.br']
+    f = open(
+        "D:\Caio\Projetos-Python\scrapy-cloud-cbf\cbf_cloud\jogos.json", "w"
+    ).close()
+    name = "cbf"
+    allowed_domains = ["cbf.com.br"]
     start_urls = [
-        f"https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2022/{i+1}" for i in range(1)]
+        f"https://www.cbf.com.br/amp/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/2022/{i+1}"
+        for i in range(380)
+    ]
 
     def parse(self, response):
-        numero_jogo = int(response.url.split('/')[-1].split('?')[0])
-        data_jogo = response.css('.col-xs-6 span::text').get()
-        jogo = {'numero_jogo': numero_jogo, "time_mandante": "Vasco",
-                "time_visitante": "Vasco",
-                "estadio": "São Januário",
-                "cidade": "Rio de Janeiro",
-                "estado": "RJ"}
-        response = requests.post('https://cbf-jogos.herokuapp.com/jogos/', jogo)
-        if not response.ok:
-            requests.put(f'https://cbf-jogos.herokuapp.com/jogos/{numero_jogo}/', jogo)
-        return jogo
+        jogo = ItemLoader(item=JogoItem(), response=response)
+
+        # Pega o local no formato ["Estádio", "Cidade", "Estado"]
+        # Se o local ainda não foi definido, retorna ["A definir", "A definir", "A definir"]
+        local = self.get_local(response)
+
+        # Pega a data no formato "dd/mm/yyyy"
+        # Se a data ainda não foi definida, retorna '00/00/0000'
+        data = self.get_data(response)
+
+        # Pega a hora no formato "00:00"
+        # Se a hora ainda não foi definida, retorna '00:00'
+        hora = self.get_hora(response)
+
+        jogo.add_value("numero", int(response.url.split("/")[-1]))
+        jogo.add_css("time_mandante", ".jogo-equipe-nome-completo::text")
+        jogo.add_css("time_visitante", ".jogo-equipe-nome-completo::text")
+        jogo.add_value("data", data)
+        jogo.add_value("hora", hora)
+        jogo.add_value("estadio", local[0])
+        jogo.add_value("cidade", local[1])
+        jogo.add_value("estado", local[2])
+
+        return jogo.load_item()
+
+    def get_hora(self, response):
+        hora = response.css(".m-t-15 .text-6::text").get()
+        if not hora:
+            hora = "00:00"
+        return hora
+
+    def get_data(self, response):
+        data = response.css(".col-xs-6 span::text").get()
+        if not data:
+            data = "00/00/0000"
+        return data
+
+    def get_local(self, response):
+        local = response.css(".col-xs-12 span::text").get().split(" - ")
+        if "a definir" in local[0].lower():
+            local = ["A definir"] * 3
+        return local
