5b8cf99ec3a9e465b5daae54f790bbfe8e666602
Caio Alves <caioluizalves1995@gmail.com>
2022-09-03 12:57:09 -0300

    atualizando configurações do scrapy

--- cbf_cloud/settings.py
+++ cbf_cloud/settings.py
@@ -68,7 +68,7 @@ ROBOTSTXT_OBEY = False
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 #AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
@@ -81,8 +81,8 @@ AUTOTHROTTLE_ENABLED = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 10000
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 10000
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
